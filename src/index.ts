2e68c34abec568d6e58a7a750bf33768654e88ac
nade <88284830+gn1e@users.noreply.github.com>
2025-06-01 00:06:22 +0300

    remove th broken stuff

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,6 @@ dotenv.config();
 const data = minecraftData(version);
 const loginPacket = data.loginPacket;
 
-function textHandler (text: string) {
-    return data.supportFeature('chatPacketsUseNbtComponents')
-      ? nbt.comp({ text: nbt.string(text) })
-      : JSON.stringify({ text })
-}
-
 server.on('playerJoin', function (client) {
     logger.information(`${client.username} has connected!`);
     client.on('end', function () {
@@ -55,59 +49,8 @@ server.on('playerJoin', function (client) {
         pitch: 0,
         flags: 0x00,
       });
-
-      function chatHandler (data: { message: string }) {
-        const message = `<${client.username}> ${data.message}`;
-        broadcast(message, null, client.username)
-        logger.information(message)
-      }
-
-      client.on('chat', chatHandler)
 });
 
 server.on('listening', function () {
     logger.information('Server listening on port 25565') // lets just assume that its listening on 25565
 })
-
-
-function sendBroadcastMessage(
-    server: Server,
-    clients: ServerClient[],
-    message: string,
-    sender: string
-  ) {
-
-    const uuid = uuidv4();
-    if (data.supportFeature('signedChat')) {
-      server.writeToClients(clients, 'player_chat', {
-        plainMessage: message,
-        signedChatContent: '',
-        unsignedChatContent: textHandler(message),
-        type: 0,
-        senderUuid: uuid,
-        senderName: JSON.stringify({ text: sender }),
-        senderTeam: undefined,
-        timestamp: Date.now(),
-        salt: 0n,
-        signature: data.supportFeature('useChatSessions') ? undefined : Buffer.alloc(0),
-        previousMessages: [],
-        filterType: 0,
-        networkName: JSON.stringify({ text: sender }),
-      });
-    } else {
-      server.writeToClients(clients, 'chat', {
-        message: JSON.stringify({ text: message }),
-        position: 0,
-        sender: sender || '0',
-      });
-    }
-  }
-  
-  function broadcast(
-    message: string,
-    exclude: ServerClient | null,
-    username: string
-  ) {
-    const recipients = Object.values(server.clients).filter(client => client !== exclude);
-    sendBroadcastMessage(server, recipients, message, username);
-  }
